e266b2db6a12147ba5cddebeb21e24f5f517afe4
Spike Cui <kmomuphnie@gmail.com>
2017-08-21 13:04:50 -0400

    Add files via upload

---
+++ cs50 syllabus.c
@@ -0,0 +1,48 @@
+9 problem sets & 1 final project
+	70%+ on every set
+
+reference:
+C Programming Absolute Beginner’s Guide, Third Edition
+Greg Perry, Dean Miller
+Pearson Education, 2014
+ISBN 0-789-75198-4
+
+Hacker’s Delight, Second Edition
+Henry S. Warren Jr.
+Pearson Education, 2013
+ISBN 0-321-84268-5
+How Computers Work, Tenth Edition
+Ron White
+Que Publishing, 2014
+ISBN 0-7897-4984-X
+
+Programming in C, Fourth Edition
+Stephen G. Kochan
+Pearson Education, 2015
+ISBN 0-321-77641-0
+
+
+Week 0
+Scratch
+Week 1
+C
+Week 2
+Arrays
+Week 3
+Algorithms
+Week 4
+Memory
+Week 5
+Data Structures
+Week 6
+HTTP
+Week 7
+Machine Learning
+Week 8
+Python
+Week 9
+SQL
+Week 10
+JavaScript
+Week 11
+The End
